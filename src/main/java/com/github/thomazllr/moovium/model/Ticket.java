522b05a141a60dfe17cf3eb02f2359e8af727365
thomazllr <guithomaz270@gmail.com>
2025-02-06 14:04:32 -0300

    add: ticket model

--- src/main/java/com/github/thomazllr/moovium/model/Ticket.java
+++ src/main/java/com/github/thomazllr/moovium/model/Ticket.java
@@ -11,14 +11,76 @@ public class Ticket {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-        @OneToOne
-        private Session session;
+    @OneToOne
+    @JoinColumn(name = "session_id")
+    private Session session;
 
-        private String seatNumber;
+    private String seatNumber;
 
-        @Column(precision = 18, scale = 2)
-        private BigDecimal price;
+    @Column(precision = 18, scale = 2)
+    private BigDecimal price;
 
-        private boolean isSold;
+    private boolean isSold;
 
+    private String qrCode;
+
+    public Ticket() {
+    }
+
+    public Ticket(UUID id, Session session, String seatNumber, BigDecimal price, boolean isSold, String qrCode) {
+        this.id = id;
+        this.session = session;
+        this.seatNumber = seatNumber;
+        this.price = price;
+        this.isSold = isSold;
+        this.qrCode = qrCode;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isSold() {
+        return isSold;
+    }
+
+    public void setSold(boolean sold) {
+        isSold = sold;
+    }
+
+    public String getQrCode() {
+        return qrCode;
+    }
+
+    public void setQrCode(String qrCode) {
+        this.qrCode = qrCode;
+    }
 }
--- src/main/resources/db/migration/V3__create_table_ticket.sql
+++ src/main/resources/db/migration/V3__create_table_ticket.sql
@@ -4,5 +4,6 @@ CREATE TABLE ticket (
                         seat_number VARCHAR(255) NOT NULL,
                         price DECIMAL(18, 2) NOT NULL,
                         is_sold BOOLEAN NOT NULL,
+                        qr_code VARCHAR(255) NOT NULL,
                         FOREIGN KEY (session_id) REFERENCES session(id)
 );
